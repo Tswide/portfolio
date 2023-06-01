f176e53ca217fb8f8cf098d095f18a71a13bb4d0
Tswide <julienjcbs@protonmail.com>
2023-06-01 15:43:56 +0200

    ajout de la db

---
+++ portfolio_ecole.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 01 juin 2023 à 13:43
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.1.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `portfolio_ecole`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_admin`
+--
+
+DROP TABLE IF EXISTS `user_admin`;
+CREATE TABLE IF NOT EXISTS `user_admin` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `mdp` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `user_admin`
+--
+
+INSERT INTO `user_admin` (`id`, `pseudo`, `mdp`) VALUES
+(1, 'root', 'root'),
+(2, 'root', 'root');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
